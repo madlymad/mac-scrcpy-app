38f231fd9513bcff361489950f1c0438511eab0d
Mando Stam <madlymad@users.noreply.github.com>
2022-02-14 12:33:45 +0200

    Update list_devices.sh
    Display device details

--- scripts/list_devices.sh
+++ scripts/list_devices.sh
@@ -1,8 +1,7 @@
-#!/bin/bash
 export ANDROID_SDK_HOME=~/Library/Android/sdk
 export PATH=${PATH}:${ANDROID_SDK_HOME}/platform-tools
 
 adb kill-server
 adb start-server
 
-adb devices | grep -w 'device' | cut -f1
+adb devices | grep -w 'device' | cut -f1 | xargs -I X sh -c 'c=$(adb -s X shell getprop ro.product.manufacturer);m=$(adb -s X shell getprop ro.product.model);s=$(echo X);echo "$s ($c $m)"'
