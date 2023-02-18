a8279532207ac21ce5bd465886f0553f720ead23
Mando Stam <madlymad@users.noreply.github.com>
2023-02-18 11:25:18 +0200

    Comment adb restart as it no longer help

--- scripts/list_devices.sh
+++ scripts/list_devices.sh
@@ -2,7 +2,9 @@
 export ANDROID_SDK_HOME=~/Library/Android/sdk
 export PATH=${PATH}:${ANDROID_SDK_HOME}/platform-tools
 
-adb kill-server
-adb start-server
+# Restart ADB, uncomment if your adb has trouble to locate the devices
+# Newer adb versions work without restaring olders require it.
+# adb kill-server
+# adb start-server
 
 adb devices | grep -w 'device' | cut -f1 | xargs -I X sh -c 'c=$(adb -s X shell getprop ro.product.manufacturer);m=$(adb -s X shell getprop ro.product.model);s=$(echo X);echo "$s ($c $m)"'
