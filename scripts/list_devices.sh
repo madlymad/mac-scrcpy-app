eaedcdcd7bef70c847475f01565ca05f7d41b83a
Mando Stamelaki <656323+madlymad@users.noreply.github.com>
2024-10-25 21:44:25 +0300

    Display dialog on no devices

--- scripts/list_devices.sh
+++ scripts/list_devices.sh
@@ -7,4 +7,14 @@ export PATH=${PATH}:${ANDROID_SDK_HOME}/platform-tools
 # adb kill-server
 # adb start-server
 
-adb devices | grep -w 'device' | cut -f1 | xargs -I X sh -c 'c=$(adb -s X shell getprop ro.product.manufacturer);m=$(adb -s X shell getprop ro.product.model);s=$(echo X);echo "$s ($c $m)"'
+# Get device list with manufacturer and model info
+device_list=$(adb devices | grep -w 'device' | cut -f1 | xargs -I X sh -c 'c=$(adb -s X shell getprop ro.product.manufacturer); m=$(adb -s X shell getprop ro.product.model); s=$(echo X); echo "$s ($c $m)"')
+
+# Check if device_list is empty
+if [ -z "$device_list" ]; then
+  # Show an Automator dialog for no devices found and exit
+  osascript -e 'display dialog "No devices found. Please connect an Android device and try again." buttons {"Exit"} default button "Exit"' >/dev/null 2>&1
+else
+  # Pass device list to the next step
+  echo "$device_list"
+fi
